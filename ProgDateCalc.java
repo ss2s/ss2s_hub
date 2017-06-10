b3d3d7eb1be05a99240fe144accdd32bc1834161
Sanya <ss.samsung86@gmail.com>
2017-06-10 17:04:46 +0300

    +

--- ProgDateCalc.java
+++ ProgDateCalc.java
@@ -15,21 +15,54 @@ c = 0;
 public ProgDateCalc () {}
 
 public void nSet (int nf) {
+
 	this.n = nf;
 }
 
-public void numSet (double af,bf,cf) {
+public void abcSet (double af,bf,cf) {
+
 	this.a = af;
 	this.b = bf;
 	this.c = cf;
 }
 
-public void charSet (char znak1f,znak2f) {
+public void aSet (double af) {
+
+	this.a = af;
+}
+
+public void bSet (double bf) {
+
+	this.b = bf;
+}
+
+public void cSet (double cf) {
+
+	this.c = cf;
+}
+
+public void znakSet (char znak1f,znak2f) {
+
 	this.znak1 = znak1f;
 	this.znak2 = znak2f;
+	this.z1 = znak1f;
+	this.z2 = znak2f;
+}
+
+public void znak1Set (char znak1f) {
+
+	this.znak1 = znak1f;
+	this.z1 = znak1f;
+}
+
+public void znak2Set (char znak2f) {
+
+	this.znak2 = znak2f;
+	this.z2 = znak2f;
 }
 
 public void startProg () {
+
 	if (a <= maxVal) {
 		while (a <= maxVal) {
 			//cout << "N "<<counter<<".     "<<a<<endl;
@@ -41,7 +74,23 @@ public void startProg () {
 	}
 }
 
-public int findDay () {}
+public int findDay () {
+
+	int dd = 32, mm = 13, yyyy = -1;
+	int nyyyy, nmm, iday, day, flag = 0;
+
+	/*string month[] = {"January", "February",
+					  "March", "April", "May",
+					 "June", "July", "August",
+                      "September", "October",
+                      "November" ,"December"};  */
+
+    dd = a;
+    mm = b;
+    yyyy = c;
+
+
+}
 
 public double startCalc () {}
 
