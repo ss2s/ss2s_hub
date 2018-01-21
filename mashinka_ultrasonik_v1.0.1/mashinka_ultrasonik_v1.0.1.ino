45624f76903e0237239fef912b1d8d7ee1eeb253
Sanya <ss.samsung86@gmail.com>
2018-01-21 23:51:23 +0200

    v210118_2351

--- mashinka_ultrasonik_v1.0.1/mashinka_ultrasonik_v1.0.1.ino
+++ mashinka_ultrasonik_v1.0.1/mashinka_ultrasonik_v1.0.1.ino
@@ -1,12 +1,25 @@
-          /* ПРОШИВКА ДЛЯ МАШИНКИ v1.0.3 Arduino ProMini */
+          /* ПРОШИВКА ДЛЯ МАШИНКИ v1.0.3 Arduino ProMini with UNO bootloader */
 
-// СТАРТОВЫЙ КОНФИГ C++
+// распиновка Arduino ProMini
+#define pin_dirLeft  11  // 11 pin направление левых колес 1↑ 0↓
+#define pin_dirRight  8  // 8 pin направление правых колес 1↑ 0↓
+#define pin_pwmLeft  10  // 10 pin скорость левых колес ШИМ 0-255
+#define pin_pwmRight  9  // 9 pin скорость правых колес ШИМ 0-255
+#define pin_voltBat   4  // A4 pin вольтметр батареи
+
+#define trigPin  2       // ultrasonik
+#define echoPin  3       // ultrasonik
+
+// макросы для двигателей
+
 // ДВИГАТЕЛИ
+// СТАРТОВЫЙ КОНФИГ
 #define DEF_START_PIN_CONFIG_DEF DDRB |= B1111;    // pinMode(11,10,9,8,OUTPUT)  // установить как выход
 #define DEF_START_DIR_SET_DEF PORTB |= B1001;      // digitalWrite(11,8,HIGH)    // направление вперед
 #define DEF_START_SPD_SET_DEF PORTB &= B11111001;  // digitalWrite(10,9,LOW)     // скорость 0
 
-// УПРАВЛЕНИЕ КОЛЕСАМИ C++
+// УПРАВЛЕНИЕ КОЛЕСАМИ
+
 // ЛЕВЫЕ И ПРАВЫЕ
 #define DEF_FORWARD_DIR_DEF PORTB |= B1001;        // digitalWrite(11,8,HIGH)    // направление вперед
 #define DEF_BACKWARD_DIR_DEF PORTB &= B11110110;   // digitalWrite(11,8,LOW)     // направление назад
@@ -14,32 +27,22 @@
 #define DEF_SPD_L_DEF PORTB &= B11111001;          // digitalWrite(10,9,LOW)     // скорость 0
 
 // ЛЕВЫЕ
-#define DEF_FORWARD_DIR_LEFT_DEF                    digitalWrite(11,HIGH)       // направление вперед левых
-#define DEF_BACKWARD_DIR_LEFT_DEF                   digitalWrite(11,LOW)        // направление назад левых
-#define DEF_SPD_H_LEFT_DEF                          digitalWrite(10,HIGH)       // скорость 255 левых
-#define DEF_SPD_L_LEFT_DEF                          digitalWrite(10,LOW)        // скорость 0 левых
+#define DEF_FORWARD_DIR_LEFT_DEF PORTB = 1<<3;     // digitalWrite(11,HIGH)       // направление вперед левых
+#define DEF_BACKWARD_DIR_LEFT_DEF PORTB = 0<<3;    // digitalWrite(11,LOW)        // направление назад левых
+#define DEF_SPD_H_LEFT_DEF PORTB = 1<<2;           // digitalWrite(10,HIGH)       // скорость 255 левых
+#define DEF_SPD_L_LEFT_DEF PORTB = 0<<2;           // digitalWrite(10,LOW)        // скорость 0 левых
 
 // ПРАВЫЕ
-#define DEF_FORWARD_DIR_RIGHT_DEF                   digitalWrite(8,HIGH)        // направление вперед правых
-#define DEF_BACKWARD_DIR_RIGHT_DEF                  digitalWrite(8,LOW)         // направление назад правых
-#define DEF_SPD_H_RIGHT_DEF                         digitalWrite(9,HIGH)        // скорость 255 правых
-#define DEF_SPD_L_RIGHT_DEF                         digitalWrite(9,LOW)         // скорость 0 правых
+#define DEF_FORWARD_DIR_RIGHT_DEF PORTB = 1<<0;    // digitalWrite(8,HIGH)        // направление вперед правых
+#define DEF_BACKWARD_DIR_RIGHT_DEF PORTB = 0<<0;   // digitalWrite(8,LOW)         // направление назад правых
+#define DEF_SPD_H_RIGHT_DEF PORTB = 1<<1;          // digitalWrite(9,HIGH)        // скорость 255 правых
+#define DEF_SPD_L_RIGHT_DEF PORTB = 0<<1;          // digitalWrite(9,LOW)         // скорость 0 правых
 
 // ULTRASONIK ДАТЧИК РАССТОЯНИЯ
 #define DEF_START_PIN_USTPO_CONFIG_DEF DDRD |= B10;        // pinMode(1,OUTPUT);  // установить как выход
 #define DEF_START_PIN_USEPI_CONFIG_DEF DDRD &= B11111110;  // pinMode(0,INPUT);   // установить как вход
-// КОНЕЦ КОНФИГА C++
+// конец макросов для двигателей
           
-// распиновка Arduino ProMini
-const byte pin_dirLeft = 11;  // 11 pin направление левых колес 1↑ 0↓
-const byte pin_dirRight = 8;  // 8 pin направление правых колес 1↑ 0↓
-const byte pin_pwmLeft = 10;  // 10 pin скорость левых колес ШИМ 0-255
-const byte pin_pwmRight = 9;  // 9 pin скорость правых колес ШИМ 0-255
-const byte pin_voltBat  = 4;  // A4 pin вольтметр батареи
-
-const byte trigPin = 2;       // ultrasonik
-const byte echoPin = 3;       // ultrasonik
-
 // переменные управленияи пинами
 boolean dirLeft = HIGH;       // направление левых колес 1↑ 0↓ HIGH or LOW
 boolean dirRight = HIGH;      // направление правых колес 1↑ 0↓ HIGH or LOW
@@ -47,9 +50,12 @@ byte pwmLeft = 0;             // скорость левых колес ШИМ 0
 byte pwmRight = 0;            // скорость правых колес ШИМ 0-255
 boolean digitalLeft = LOW;    // разрешение движения слева 1↑↓ 0x применяется к пину pin_pwmLeft     // alternative
 boolean digitalRight = LOW;   // разрешение движения справа 1↑↓ 0x применяется к пину pin_pwmRight   // alternative
+byte minSpeed = 150;          // минимальная скорость движения
+int stopDistance = 50;        // дистанция остановки
 
 // переменные управления
 boolean enMove = false;       // разрешение движения
+int iSpd;
 
 // переменные для вычислений
 long duration, cm, lcm;       // ultrasonik
@@ -68,7 +74,7 @@ void fBlink(int qnt = 1){
 	}
 }
 
-void fExtBlink(int qnt = 1, dl1 = 200, dl2 = 500){
+void fExtBlink(int qnt = 1, int dl1 = 200, int dl2 = 500){
 	int bQnt = qnt, bDl1 = dl1, bDl2 = dl2;
 	for(int i; i > bQnt; i --){
 		digitalWrite(LED_BUILTIN, HIGH);
@@ -102,6 +108,50 @@ int dist(){  // функция проверки расстояния пред с
 	return cm;
 }
 
+void mashinaRider(int dir = 1, int spd = 1){
+	int rDir = dir;
+	int rSpd = spd;
+	if(rDir == 1){
+		DEF_FORWARD_DIR_DEF;
+	}else{
+		DEF_BACKWARD_DIR_DEF;
+	}
+
+	if(rSpd == 1){
+		DEF_SPD_H_DEF;
+	}else if(rSpd == 0){
+		DEF_SPD_L_DEF;
+	}else if(rSpd > 1){
+		if(rSpd < minSpeed){
+			rSpd = minSpeed;
+		}else if(rSpd > 255){
+			rSpd = 255;
+		}
+		analogWrite(pin_pwmLeft, rSpd);
+		analogWrite(pin_pwmRight, rSpd);
+	}
+}
+
+void mashinaRotate(){}
+
+void randRotate(){}
+
+void robotRider(void){
+	int distance = dist();
+}
+
+void robotRiderTest(void){
+	int distance = dist();
+	int stopDistance2 = stopDistance + 10;
+	int rrtSpd = 0;
+	if(distance < stopDistance){
+		rrtSpd = 0;
+	}else if(distance >= (stopDistance2)){
+		rrtSpd = 1;
+	}
+	mashinaRider(1, rrtSpd);
+}
+
 void setup() {
 
 	DEF_START_PIN_CONFIG_DEF;  // установка управляющих пинов (8,9,10,11) на выход = output = 1;
@@ -114,11 +164,14 @@ void setup() {
 	pinMode(trigPin, OUTPUT);
 	pinMode(echoPin, INPUT);
 	pinMode(LED_BUILTIN, OUTPUT);
-  
 }
 
 void loop() {
 
+	//mashinaRider();
+	//robotRider();
+	robotRiderTest();
+	delay(200);
 }
 
 
