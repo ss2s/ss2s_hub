8d9f84e6e93fbdd998ab6f30385bec3d83c6772b
Sanya <ss.samsung86@gmail.com>
2018-03-02 02:04:30 +0200

    add type in for KSO

--- IcebergGroup/KSO_2054_V1/KSO_2054_ARDUINO/KSO_2054_ARDUINO.ino
+++ IcebergGroup/KSO_2054_V1/KSO_2054_ARDUINO/KSO_2054_ARDUINO.ino
@@ -11,9 +11,44 @@
 #define HIGH_PRESSURE_PUMP_RELE_PIN_OUT 10  // выход для включения/выключения насоса высокого давления (нормально открытый, пассивный)
 #define WASH_GATE_RELE_PIN_OUT 11           // выход для открытия/закрытия клапана промывки (нормально открытый, пассивный)
 #define CHEMICAL_WASHING_PUMP 12            // выход для включения/выключения насоса химической промывки (нормально открытый, пассивный)
+// ТИП ВХОДА (НОРМАЛЬНО ЗАМКНУТЫЙ, НОРМАЛЬНО РАЗОМКНУТЫЙ)
+bool lowPressureSensorTypeIn = 1;
+bool highPressureSensorTypeIn = 0;
+bool tankFillSensorTypeIn = 1;
+bool externalLockoutSensorTypeIn = 0;
+bool chemicalWashSensorTypeIn = 1;
 
 void setup() {
 }
 
 void loop() {
 }
+
+/*void sendToLCD(uint8_t type, String index, String cmd)
+{
+     if (type == 1 ){
+        Nextion.print(index);
+        Nextion.print(".txt=");
+        Nextion.print("\"");
+        Nextion.print(cmd);
+        Nextion.print("\"");
+    }
+    else if (type == 2){
+        Nextion.print(index);
+        Nextion.print(".val=");
+        Nextion.print(cmd);
+    }
+    else if (type == 3){
+        Nextion.print(index);
+        Nextion.print(".picc="); 
+        Nextion.print(cmd);
+    }
+    else if (type ==4 ){
+        Nextion.print("page ");
+        Nextion.print(cmd);
+    }
+
+    Nextion.write(0xff);
+    Nextion.write(0xff);
+    Nextion.write(0xff);
+}*/
